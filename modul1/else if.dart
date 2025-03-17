2dd2a2bd637f6615de4bbac75f701511dbc6eb68
aryanfaldu <58204215+aryanfaldu@users.noreply.github.com>
2025-03-17 17:30:11 +0530

    Add files via upload

---
+++ modul1/else if.dart
@@ -0,0 +1,33 @@
+void main ()
+{
+  int marks = 39;
+
+  if (marks>=90)
+    {
+      print('A+');
+    }
+  else if (marks>=80)
+  {
+    print('A');
+  }
+ else if (marks>=70)
+  {
+    print('B+');
+  }
+  else if (marks>=60)
+  {
+    print('B');
+  }
+  else if (marks>=50)
+  {
+    print('C');
+  }
+  else if (marks>=40)
+  {
+    print('D');
+  }
+  else
+  {
+    print('Fail');
+  }
+}
---
+++ modul1/if else.dart
@@ -0,0 +1,14 @@
+void main ()
+{
+ int A=77;
+
+ if(A>=50)
+   {
+     print('Pass');
+   }
+ else
+   {
+     print('fail');
+   }
+
+}
